3245ec29aec6363eb6b614b58ed54b8f03bb55b9
idan <idankob@gmail.com>
2024-02-16 21:53:34 +0200

    Added unit testing for jda listener auto register

---
+++ src/test/java/com/github/idankoblik/quark/listeners/OnReadyListenerTest.java
@@ -0,0 +1,13 @@
+package com.github.idankoblik.quark.listeners;
+
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class OnReadyListenerTest extends ListenerAdapter {
+
+    @Override
+    public void onReady(@NotNull ReadyEvent event) {
+
+    }
+}
